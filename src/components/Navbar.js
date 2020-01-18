2d035daa72d4d49f26e1e8df411bb41c8f57b3a4
ykurata <yasukokurata@gmail.com>
2020-01-18 15:30:26 -0500

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
+      <div className="container-fluid">
+        {/* <a className="navbar-brand" href="#">Yasuko</a> */}
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Skills</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Projects</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
