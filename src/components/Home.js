22f08d59677c417b4f7be252b15b782f553a570e
ykurata <yasukokurata@gmail.com>
2020-02-01 11:43:37 -0500

    fixed typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
     <div id="home" style={myStyle}>
       <div className="landing-text">
         <h1>Yasuko Kurata</h1>
-        <h3>Web developer who's passionate aboyt coding!</h3>
+        <h3>Web developer who's passionate about coding!</h3>
       </div>
     </div>
   );
