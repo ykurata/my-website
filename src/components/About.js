e5b5c29ffb57f3e9e5f8e1b029f181ec01163834
ykurata <yasukokurata@gmail.com>
2020-05-28 15:41:25 -0400

    removed animation from about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,27 @@
 import React from "react";
-import Slide from 'react-reveal/Slide';
 
 const About = () => {
   return (
     <div id="about" className="container padding offset">
       <div className="narrow">
-        <Slide bottom>
-          <div className="col-lg-12 text-center">
-            <h2 className="heading">About Me</h2>
-            <div className="heading-underline"></div>
-            <div className="text-left">
-              <p>Hi I'm Yasuko Kurata, web developer who's looking for an opportunity to join a great team!</p>
-              <p>
-                I started my coding journey through the online tech school <a href="https://teamtreehouse.com">Team Treehouse</a> where I completed both the &nbsp;
-                <strong>Full Stack Javascript Techdegree</strong> and the <strong>Python Web Development Techdegree</strong>.
-                Recently, I completed <strong>Hatchways Project-Based Development Course</strong>, where I worked within a team, and learned utilized git flow
-                and issue tracking.
-              </p>
-              <p>
-                I'm experienced in using various languages and frameworks such as NodeJS, Express, React, Python and Django,
-                along with understanding of relational and non-relational databases.
-                I continue to learn new skills by building my own projects and doing daily code challenges. 
-              </p>
-            </div>
+        <div className="col-lg-12 text-center">
+          <h2 className="heading">About Me</h2>
+          <div className="heading-underline"></div>
+          <div className="text-left">
+            <p>Hi I'm Yasuko Kurata, web developer who's looking for an opportunity to join a great team!</p>
+            <p>
+              I started my coding journey through the online tech school <a href="https://teamtreehouse.com">Team Treehouse</a> where I completed both the
+              Full Stack Javascript Techdegree and the Python Web Development Techdegree.
+              Recently, I completed Hatchways Project-Based Development Course, where I worked within a team, and learned utilized git flow
+              and issue tracking.
+            </p>
+            <p>
+              I'm experienced in using various languages and frameworks such as NodeJS, Express, React, Python and Django,
+              along with understanding of relational and non-relational databases.
+              I continue to learn new skills by building my own projects and doing daily code challenges. 
+            </p>
           </div>
-        </Slide>
+        </div>
       </div>
     </div>
   );
