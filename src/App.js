4758c3099d56bb4b7ef4cfd6d9353970aa366f35
ykurata <yasukokurata@gmail.com>
2020-08-26 16:03:46 -0400

    updated home components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import Home from './components/Home';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,30 @@
 import React from "react";
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 import profile from "../image/yasuko_crop9.jpg";
-
-// import components
-import Navbar from './Navbar';
-import About from "./About";
-import Skills from "./Skills";
-import Projects from "./Projects";
-import Contact from "./Contact";
 
 const homeStyles = {
   backgroundImage: `url(${profile})`,
-  backgroundSize: 'cover',
-  height: '100vh',
-  width: '100%',
-  position: 'relative',
-  display: 'table',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center right',
-  backgroundAttachment: 'fixed'
-}
-
+  backgroundSize: "cover",
+  height: "100vh",
+  width: "100%",
+  position: "relative",
+  display: "table",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center right",
+  backgroundAttachment: "fixed",
+};
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
       <section id="home" style={homeStyles}>
-       
-        <div className="landing-text" >
+        <div className="landing-text">
           <Fade top>
             <h1>Yasuko Kurata</h1>
-            <h3>Web developer who's passionate about coding!</h3>
+            <h3>Web developer who's passionate about coding</h3>
           </Fade>
         </div>
-       
       </section>
-      <About/>
-      <Skills />
-      <Projects />
-      <Contact />
     </div>
   );
 };
